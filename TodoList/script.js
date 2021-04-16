01eb67c97dfa2bab555e6dc97fc3e4cc55ec61ba
Arafat <arafat97alim@gmail.com>
2021-04-16 15:01:49 +0530

    Ui changes and major feature added to my TODOLIST v.0.0.1

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,6 +1,32 @@
 //console.log("WelcomeHome")
-var btnAdd = document.getElementById("add-item");
+
+var btnAdd = document.getElementById("add-item"); // making my button responsive on click , here access var
+var list = document.getElementById("addList");
+var inputElement = document.getElementById('todoInput');
+
+var currentInput = ""; // empty string
+inputElement.value = ""; //clearing my input box
+//function for current input
+inputElement.addEventListener('input', function(e){
+    currentInput = (e.target.value);
+})
 
 btnAdd.addEventListener('click', function(){
-    alert("Welcome User")
+    //alert("Welcome User")
+    var newElementList = document.createElement('li'); // create a new list by taking tag as a 'li'
+
+    newElementList.id = "item- " + (list.childElementCount + 1); //adding dynamic id to my list 
+
+    //var textNode = document.createTextNode("List Item " + (list.childElementCount)); //making list value
+    var textNode = document.createTextNode(currentInput);
+
+    newElementList.appendChild(textNode);
+    list.appendChild(newElementList)
+    
+    //list count
+    //newElementList= list.childElementCount + 1;
+    console.log(newElementList)
+    inputElement.value = ''; //clear my input box 
+    currentInput = ''; //clear my input value
+
 })
