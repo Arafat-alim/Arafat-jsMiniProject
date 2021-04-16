fc1aa968fc379d552891e75cd31c9631bbce4bd5
Arafat <arafat97alim@gmail.com>
2021-04-16 15:24:25 +0530

    ****fixing bugs and improvement to my TODOLIST

--- TodoList/script.js
+++ TodoList/script.js
@@ -4,29 +4,44 @@ var btnAdd = document.getElementById("add-item"); // making my button responsive
 var list = document.getElementById("addList");
 var inputElement = document.getElementById('todoInput');
 
+
 var currentInput = ""; // empty string
 inputElement.value = ""; //clearing my input box
+
+//Enter CLicked Feature
+inputElement.addEventListener('keyup', function(e){
+    if(e.keyCode === 13){
+        //alert("Enter Button is Pressed")
+        addListItem(); //function calling
+    }
+})
 //function for current input
 inputElement.addEventListener('input', function(e){
     currentInput = (e.target.value);
 })
 
-btnAdd.addEventListener('click', function(){
-    //alert("Welcome User")
-    var newElementList = document.createElement('li'); // create a new list by taking tag as a 'li'
-
-    newElementList.id = "item- " + (list.childElementCount + 1); //adding dynamic id to my list 
-
-    //var textNode = document.createTextNode("List Item " + (list.childElementCount)); //making list value
-    var textNode = document.createTextNode(currentInput);
-
-    newElementList.appendChild(textNode);
-    list.appendChild(newElementList)
+function addListItem(){
+    if(currentInput !== null && currentInput !== undefined && currentInput !== ""){
+        //alert("Welcome User")
+            var newElementList = document.createElement('li'); // create a new list by taking tag as a 'li'
     
-    //list count
-    //newElementList= list.childElementCount + 1;
-    console.log(newElementList)
-    inputElement.value = ''; //clear my input box 
-    currentInput = ''; //clear my input value
+            newElementList.id = "item- " + (list.childElementCount + 1); //adding dynamic id to my list 
+    
+            //var textNode = document.createTextNode("List Item " + (list.childElementCount)); //making list value
+            var textNode = document.createTextNode(currentInput);
+    
+            newElementList.appendChild(textNode); //adding my list item at the end 
+            list.appendChild(newElementList) //adding my newELementlist in my unoderedList
+        
+            //list count
+            //newElementList= list.childElementCount + 1;
+    
+            //console.log(newElementList)
+            inputElement.value = ''; //clear my input box 
+            currentInput = ''; //clear my input value
+        }else{
+            alert("Please Enter Valid TODO Item ")
+        }    
+}
 
-})
+btnAdd.addEventListener('click', addListItem);
