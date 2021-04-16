39adb223c162fb15ef85388933947604709763bb
Arafat <arafat97alim@gmail.com>
2021-04-16 16:28:05 +0530

    Added feature button like updating and deleting and improvments v.0.1.0

--- TodoList/script.js
+++ TodoList/script.js
@@ -3,6 +3,8 @@
 var btnAdd = document.getElementById("add-item"); // making my button responsive on click , here access var
 var list = document.getElementById("addList");
 var inputElement = document.getElementById('todoInput');
+var updateList = document.getElementById("replace-item");
+var deleteList = document.getElementById("delete-item");
 
 
 var currentInput = ""; // empty string
@@ -19,23 +21,23 @@ inputElement.addEventListener('keyup', function(e){
 inputElement.addEventListener('input', function(e){
     currentInput = (e.target.value);
 })
+function newListNode(){ //creating new list codes 
+    var newElementList = document.createElement('li'); // create a new list by taking tag as a 'li'
+    newElementList.id = "item- " + (list.childElementCount + 1); //adding dynamic id to my list 
+    //var textNode = document.createTextNode("List Item " + (list.childElementCount)); //making list value
+    var textNode = document.createTextNode(currentInput);
+    newElementList.appendChild(textNode); //adding my list item at the end 
+
+    return newElementList;
+}
 
 function addListItem(){
     if(currentInput !== null && currentInput !== undefined && currentInput !== ""){
         //alert("Welcome User")
-            var newElementList = document.createElement('li'); // create a new list by taking tag as a 'li'
-    
-            newElementList.id = "item- " + (list.childElementCount + 1); //adding dynamic id to my list 
-    
-            //var textNode = document.createTextNode("List Item " + (list.childElementCount)); //making list value
-            var textNode = document.createTextNode(currentInput);
-    
-            newElementList.appendChild(textNode); //adding my list item at the end 
+            var newElementList = newListNode(); //calling func for creating a newlist
             list.appendChild(newElementList) //adding my newELementlist in my unoderedList
-        
             //list count
             //newElementList= list.childElementCount + 1;
-    
             //console.log(newElementList)
             inputElement.value = ''; //clear my input box 
             currentInput = ''; //clear my input value
@@ -45,3 +47,22 @@ function addListItem(){
 }
 
 btnAdd.addEventListener('click', addListItem);
+
+//updateFirstList
+updateList.addEventListener('click', function(){
+    var firstElement = list.firstElementChild; //old element access
+    var newElementList = newListNode(); //new element
+
+    //Replace Function accept two arguements - newlist and replace element
+    list.replaceChild(newElementList, firstElement);  
+    inputElement.value = "";
+    currentInput = "";
+    
+})
+
+
+//deleteFirstList
+deleteList.addEventListener('click',function(){
+    var firstElement = list.firstElementChild;
+    list.removeChild(firstElement); 
+})
