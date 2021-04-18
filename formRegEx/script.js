2388231c1bb8c992661ef91ce50825c8a4b0e4fb
Arafat <arafat97alim@gmail.com>
2021-04-18 15:26:45 +0530

    **Stable - Added major Features, fix bugs, improvements

--- formRegEx/script.js
+++ formRegEx/script.js
@@ -1,1 +1,15 @@
-console.log('HelloWorld')
+//console.log('HelloWorld')
+var inputUsername = document.getElementById("username");
+var para = document.getElementById("paraText")
+inputUsername.addEventListener("input", function (e) {
+    var pattern = /^[\w]{6,8}$/;
+    var currentVal = e.target.value;
+    var valid = pattern.test(currentVal);
+
+    if (valid) {
+        para.style.display = 'none';
+    } else {
+        para.style.display = 'block';
+    }
+
+})
