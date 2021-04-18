ba9b8c6cd5d26578b12cee8a701b24d9a32de3a9
Arafat <arafat97alim@gmail.com>
2021-04-18 20:01:49 +0530

    ***Beta-- Added major features, fixed bugs and Improvements.

--- checkValidURL/script.js
+++ checkValidURL/script.js
@@ -1,1 +1,15 @@
-console.log("Checking URL ... ")
+//console.log("Checking URL ... ")
+var inputBox = document.getElementById("inputURL")
+var para = document.getElementById("paratext")
+inputBox.addEventListener("input", function (e) {
+    var currentInput = e.target.value;
+    var pattern = /^www\.[\w]+\.(co|com|org|in)$/g;
+    var valid = pattern.test(currentInput);
+
+    if (valid) {
+        para.innerHTML = "Whoopiee!!! Its a Proper Website "
+    } else {
+        para.innerHTML = "Oop! Something Error!! or Wrong URL"
+    }
+
+})
